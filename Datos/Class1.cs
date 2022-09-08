68f741ac38941e9b67b46b2743e34d16801514b1
Àngel <angel@rom.cat>
2022-09-08 17:54:02 +0200

    Agregar archivos de proyecto.

---
+++ Datos/Class1.cs
@@ -0,0 +1,84 @@
+﻿using Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Class1
+    {
+        private static UnaTablaDbContext _db = null;
+        public Class1(){
+            if (_db == null)
+            {
+                _db = new UnaTablaDbContext();
+            }
+        }   
+
+        public bool Create(persona persona)
+        {
+            _db.persona.Add(persona);
+
+            return true;    
+        }
+
+        public bool GuardarCambios()
+        {
+            bool ok = false;
+            try
+            {
+                int respuesta = 0;
+                respuesta = _db.SaveChanges();
+                if (respuesta > 0)
+                {
+                    ok = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        public IList<persona> Read(string texto)
+        {
+            IList<persona> personas = null;
+            //personas = new List<persona>();
+
+            //texto == null
+            //devolveremos todos los registros
+            personas = _db.persona.ToList();
+
+            if (texto != null && texto != "")
+            {   //texto != null && texto != ""
+                int id = 0;
+                if (int.TryParse(texto, out id) == true)
+                {   //texto == INT
+                    id = int.Parse(texto);
+
+                    
+                    if (id > 0)
+                    {//texto > 0
+                     //Devolveremos el registro con ese id
+
+                        personas = personas.Where(x => x.id == id).ToList();
+                        //persona persona = personas.Where(x => x.id == id).FirstOrDefault();
+                    }
+                }
+                else
+                {
+                    //texto != INT
+                    //Devolveremos todos los registros con ese nombre
+                    personas = personas.Where(x => x.nombre == texto).ToList();
+                }
+            }
+
+            return personas;
+        }
+    }
+}
---
+++ Principal/Program.cs
@@ -0,0 +1,77 @@
+﻿using Datos;
+using Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal
+{
+    internal class Program
+    {
+        private static Class1 _class1 = null;
+        static void Main(string[] args)
+        {
+            _class1 = new Class1();
+            Program program = new Program();
+            program.Create();
+            program.Read();
+            program.Read();
+            program.Read();
+            program.Read();
+            program.Read();
+        }
+
+        private void Create()
+        {
+            bool ok = false;
+            Console.WriteLine("Escribe el NOMBRE de la persona del nuevo registro");
+            string texto = Console.ReadLine();
+            if (texto != null && texto != "")
+            {
+                persona persona = new persona();
+                //persona.id = ??;
+                persona.nombre = texto;
+
+                Console.WriteLine("Escribe el PRIMER APELLIDO de la persona del nuevo registro");
+                texto = Console.ReadLine();
+                if (texto != null && texto != "")
+                {
+                    persona.apellido01 = texto;
+
+                    Console.WriteLine("Escribe el SEGUNDO APELLIDO de la persona del nuevo registro");
+                    texto = Console.ReadLine();
+                    if (texto != null && texto != "")
+                    {
+                        persona.apellido02 = texto;
+                    }
+
+                    ok = _class1.Create(persona);
+                    if (ok == true)
+                    {
+                        ok = _class1.GuardarCambios();
+                    }
+                }
+            }
+            Console.WriteLine("La inserción del nuevo registro ha sido: " + ok);
+            Console.ReadLine();
+        }
+
+        private void Read()
+        {
+            Console.WriteLine("Escribe el ID o el NOMBRE del registro que quieres leer");
+            string texto = Console.ReadLine();
+
+            IList<persona> personas = null;
+            //personas = new List<persona>();
+            personas = _class1.Read(texto);
+
+            foreach(persona persona in personas)
+            {
+                Console.WriteLine(persona.id + " " + persona.nombre);
+            }
+            Console.ReadLine();
+        }
+    }
+}
